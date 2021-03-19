93ec6e108222a381b0e74d452fe9d31e1d6f81d0
Luc Uppenkamp <35378496+lucasjup@users.noreply.github.com>
2021-03-19 04:54:07 +0100

    Beautified code

--- js/earthTime.js
+++ js/earthTime.js
@@ -1,50 +1,64 @@
 var timeTravel = false;
 var speedTime = 0;
-
 // changes appearance of elements based on the time
 function earthTime() {
 	var today = new Date();
 	var seconds = today.getSeconds();
 	var minutes = today.getMinutes();
 	var hour = today.getHours();
-
-  //to start time travel at the correct time
-  speedTime = hour;
-
-  // changes appearance of elements based on the time
-  if (hour >= 6 && hour < 7) {
-    // Sun rise
-    document.getElementById('earth').className = 'timeSunRise';
-    document.getElementById('earthNight').className = 'earthHide';
-    document.getElementById('earthSunLow').className = 'earthShow';
-  } else if (hour >= 7 && hour < 12) {
-    // Morning
-    document.getElementById('earth').className = 'timeMorning';
-    document.getElementById('earthSunLow').className = 'earthHide';
-    document.getElementById('earthWarm').className = 'earthShow';
-  } else if (hour >= 12 && hour < 18) {
-    // Day
-    document.getElementById('earth').className = 'timeDay';
-    document.getElementById('earthWarm').className = 'earthHide';
-    document.getElementById('earthDay').className = 'earthShow';
-  } else if (hour >= 18 && hour < 21) {
-    // Evening
-    document.getElementById('earth').className = 'timeEvening';
-    document.getElementById('earthDay').className = 'earthHide';
-    document.getElementById('earthWarm').className = 'earthShow';
-  } else if (hour >= 21 && hour < 22) {
-    // Sun set
-    document.getElementById('earth').className = 'timeSunDown';
-    document.getElementById('earthWarm').className = 'earthHide';
-    document.getElementById('earthSunLow').className = 'earthShow';
-  } else {
-    // Night
-    document.getElementById('earth').className = 'timeNight';
-    document.getElementById('earthSunLow').className = 'earthHide';
-    document.getElementById('earthNight').className = 'earthShow';
-  }
-
-  // Adds zero before 1 digit times
+	//to start time travel at the correct time
+	speedTime = hour;
+	// changes appearance of elements based on the time
+	if (hour >= 6 && hour < 7) {
+		// Sun rise
+		document.getElementById('earth')
+			.className = 'timeSunRise';
+		document.getElementById('earthNight')
+			.className = 'earthHide';
+		document.getElementById('earthSunLow')
+			.className = 'earthShow';
+	} else if (hour >= 7 && hour < 12) {
+		// Morning
+		document.getElementById('earth')
+			.className = 'timeMorning';
+		document.getElementById('earthSunLow')
+			.className = 'earthHide';
+		document.getElementById('earthWarm')
+			.className = 'earthShow';
+	} else if (hour >= 12 && hour < 18) {
+		// Day
+		document.getElementById('earth')
+			.className = 'timeDay';
+		document.getElementById('earthWarm')
+			.className = 'earthHide';
+		document.getElementById('earthDay')
+			.className = 'earthShow';
+	} else if (hour >= 18 && hour < 21) {
+		// Evening
+		document.getElementById('earth')
+			.className = 'timeEvening';
+		document.getElementById('earthDay')
+			.className = 'earthHide';
+		document.getElementById('earthWarm')
+			.className = 'earthShow';
+	} else if (hour >= 21 && hour < 22) {
+		// Sun set
+		document.getElementById('earth')
+			.className = 'timeSunDown';
+		document.getElementById('earthWarm')
+			.className = 'earthHide';
+		document.getElementById('earthSunLow')
+			.className = 'earthShow';
+	} else {
+		// Night
+		document.getElementById('earth')
+			.className = 'timeNight';
+		document.getElementById('earthSunLow')
+			.className = 'earthHide';
+		document.getElementById('earthNight')
+			.className = 'earthShow';
+	}
+	// Adds zero before 1 digit times
 	if (seconds < 10) {
 		seconds = '0' + seconds;
 	}
@@ -54,88 +68,109 @@ function earthTime() {
 	if (hour < 10) {
 		hour = '0' + hour;
 	}
-
-  // Add time to html
-	document.getElementById('earthTime').innerHTML = hour + ':' + minutes + ':' + seconds;
+	// Add time to html
+	document.getElementById('earthTime')
+		.innerHTML = hour + ':' + minutes;
 }
-
-// simulation of time passing by
+// Time Travel function
 function earthTimeSpeed() {
-  if (speedTime == 24) {
-    speedTime = 0;
-  } else if ((speedTime >= 5 && speedTime < 8) || (speedTime >= 18 && speedTime < 23)) {
-    speedTime = speedTime + 0.25;
-  } else {
-    speedTime ++;
-  };
-
-  console.log(speedTime);
-
-  // changes appearance of elements based on the simulated time
-  if (speedTime >= 6 && speedTime < 7) {
-    // Sun rise
-    document.getElementById('earth').className = 'timeSunRise';
-    document.getElementById('earthNight').className = 'earthHide';
-    document.getElementById('earthSunLow').className = 'earthShow';
-  } else if (speedTime >= 7 && speedTime < 12) {
-    // Morning
-    document.getElementById('earth').className = 'timeMorning';
-    document.getElementById('earthSunLow').className = 'earthHide';
-    document.getElementById('earthWarm').className = 'earthShow';
-  } else if (speedTime >= 12 && speedTime < 18) {
-    // Afternoon
-    document.getElementById('earth').className = 'timeDay';
-    document.getElementById('earthWarm').className = 'earthHide';
-    document.getElementById('earthDay').className = 'earthShow';
-  } else if (speedTime >= 18 && speedTime < 21) {
-    // Evening
-    document.getElementById('earth').className = 'timeEvening';
-    document.getElementById('earthDay').className = 'earthHide';
-    document.getElementById('earthWarm').className = 'earthShow';
-  } else if (speedTime >= 21 && speedTime < 22) {
-    // Sun set
-    document.getElementById('earth').className = 'timeSunDown';
-    document.getElementById('earthWarm').className = 'earthHide';
-    document.getElementById('earthSunLow').className = 'earthShow';
-  } else {
-    // Night
-    document.getElementById('earth').className = 'timeNight';
-    document.getElementById('earthSunLow').className = 'earthHide';
-    document.getElementById('earthNight').className = 'earthShow';
-  }
+	if (speedTime == 24) {
+		speedTime = 0;
+	} else if ((speedTime >= 5 && speedTime < 8) || (speedTime >= 18 && speedTime < 23)) {
+		speedTime = speedTime + 0.25;
+	} else {
+		speedTime++;
+	};
+	console.log(speedTime);
+	// changes appearance of elements based on the simulated time
+	if (speedTime >= 6 && speedTime < 7) {
+		// Sun rise
+		document.getElementById('earth')
+			.className = 'timeSunRise';
+		document.getElementById('earthNight')
+			.className = 'earthHide';
+		document.getElementById('earthSunLow')
+			.className = 'earthShow';
+	} else if (speedTime >= 7 && speedTime < 12) {
+		// Morning
+		document.getElementById('earth')
+			.className = 'timeMorning';
+		document.getElementById('earthSunLow')
+			.className = 'earthHide';
+		document.getElementById('earthWarm')
+			.className = 'earthShow';
+	} else if (speedTime >= 12 && speedTime < 18) {
+		// Afternoon
+		document.getElementById('earth')
+			.className = 'timeDay';
+		document.getElementById('earthWarm')
+			.className = 'earthHide';
+		document.getElementById('earthDay')
+			.className = 'earthShow';
+	} else if (speedTime >= 18 && speedTime < 21) {
+		// Evening
+		document.getElementById('earth')
+			.className = 'timeEvening';
+		document.getElementById('earthDay')
+			.className = 'earthHide';
+		document.getElementById('earthWarm')
+			.className = 'earthShow';
+	} else if (speedTime >= 21 && speedTime < 22) {
+		// Sun set
+		document.getElementById('earth')
+			.className = 'timeSunDown';
+		document.getElementById('earthWarm')
+			.className = 'earthHide';
+		document.getElementById('earthSunLow')
+			.className = 'earthShow';
+	} else {
+		// Night
+		document.getElementById('earth')
+			.className = 'timeNight';
+		document.getElementById('earthSunLow')
+			.className = 'earthHide';
+		document.getElementById('earthNight')
+			.className = 'earthShow';
+	}
 }
-
+// Toggles The Time Travel Variable
 function toggleTimeTravel() {
-  if (timeTravel == false) {
-    timeTravel = true;
-    document.getElementById('timeTravel').innerHTML = "<p> Real time day and night cycle </p>"
-  	document.getElementById('earthTime').innerHTML = "";
-    document.getElementById('earthDate').innerHTML = "";
-  } else {
-    timeTravel = false;
-    earthDate();
-    document.getElementById('timeTravel').innerHTML = "<p> Watch the day and night cycle </p>"
-  }
+	if (timeTravel == false) {
+		timeTravel = true;
+		document.getElementById('timeTravel')
+			.innerHTML = "Return to real time";
+		document.getElementById('timeTravel')
+			.className = 'timeTravelLarge';
+		document.getElementById('earthTime')
+			.innerHTML = "";
+		document.getElementById('earthDate')
+			.innerHTML = "";
+	} else {
+		timeTravel = false;
+		earthDate();
+		document.getElementById('timeTravel')
+			.innerHTML = "Watch the light cycle"
+	}
 }
-
+// Checks For The Time Travel Variable
+// Starts earthTime And earthTimeSpeed
 function animationTime() {
-  if (timeTravel == true) {
-    earthTimeSpeed();
-  } else {
-    earthTime();
-  }
+	if (timeTravel == true) {
+		earthTimeSpeed();
+	} else {
+		earthTime();
+	}
 }
-
+// Get And Insert Date
 function earthDate() {
-  if (timeTravel == false) {
-    var today = new Date();
-    var months = new Array('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december');
-    document.getElementById('earthDate').innerHTML = today.getDate() + ' ' + months[today.getMonth()];
-  }
+	if (timeTravel == false) {
+		var today = new Date();
+		var months = new Array('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december');
+		document.getElementById('earthDate')
+			.innerHTML = today.getDate() + ' ' + months[today.getMonth()];
+	}
 }
-
 document.onload = animationTime();
 document.onload = earthDate();
-
 setInterval(animationTime, 1000);
 setInterval(earthDate, 60000);
--- js/main.js
+++ js/main.js
@@ -4,41 +4,39 @@ var blueOrangeGrad = ctx.createLinearGradient(0, 0, 0, 150);
 blueOrangeGrad.addColorStop(0, "rgba(223, 161, 124, 1)");
 blueOrangeGrad.addColorStop(1, "rgba(40, 39, 65, 1)");
 var chartWaterUse = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: [
-        'apr',
-        'may',
-        'jun',
-        'jul',
-        'aug',
-        'sep',
-        'okt',
-        'nov',
-        'dec'
-      ],
-      datasets: [
-        {
-          label: 'H20 use in liters',
-          backgroundColor: blueOrangeGrad,
-          data: [
-            250,
-            240,
-            270,
-            290,
-            240,
-            260,
-            250,
-            270,
-            240
-          ]
-        }
+  type: 'bar',
+  data: {
+    labels: [
+      'apr',
+      'may',
+      'jun',
+      'jul',
+      'aug',
+      'sep',
+      'okt',
+      'nov',
+      'dec'
+    ],
+    datasets: [{
+      label: 'H20 use in liters',
+      backgroundColor: blueOrangeGrad,
+      data: [
+        250,
+        240,
+        270,
+        290,
+        240,
+        260,
+        250,
+        270,
+        240
       ]
-    },
-    options: {
+    }]
+  },
+  options: {
 
-    },
-  });
+  },
+});
 
 // Hamster Population
 var ctx = document.getElementById('shipChartHamster').getContext('2d');
@@ -46,42 +44,41 @@ var blueOrangeGrad = ctx.createLinearGradient(0, 0, 0, 150);
 blueOrangeGrad.addColorStop(0, "rgba(223, 161, 124, 1)");
 blueOrangeGrad.addColorStop(1, "rgba(40, 39, 65, 1)");
 var chartHamster = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'apr',
-        'may',
-        'jun',
-        'jul',
-        'aug',
-        'sep',
-        'okt',
-        'nov',
-        'dec'
-      ],
-      datasets: [{
-          label: 'Hamsters',
-          pointColor: blueOrangeGrad,
-          pointSize: 5,
-          backgroundColor: blueOrangeGrad,
-          data: [
-            10,
-            48,
-            52,
-            110,
-            150,
-            183,
-            90,
-            130,
-            150
-          ]
-        }
+  type: 'line',
+  data: {
+    labels: [
+      'apr',
+      'may',
+      'jun',
+      'jul',
+      'aug',
+      'sep',
+      'okt',
+      'nov',
+      'dec'
+    ],
+    datasets: [{
+      label: 'Hamsters',
+      pointColor: blueOrangeGrad,
+      pointSize: 5,
+      backgroundColor: blueOrangeGrad,
+      data: [
+        10,
+        48,
+        52,
+        110,
+        150,
+        183,
+        90,
+        130,
+        150
       ]
-    },
-    options: {
-      responsive: true,
-    },
-    });
+    }]
+  },
+  options: {
+    responsive: true,
+  },
+});
 
 // Travel Time & Distance
 var ctx = document.getElementById('shipChartTravelTime').getContext('2d');
@@ -95,28 +92,26 @@ var blueGrad = ctx.createLinearGradient(0, 0, 0, 400);
 blueGrad.addColorStop(0, "#44426e");
 blueGrad.addColorStop(1, "#ad627f");
 var chartTravelTime = new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: [
-        'days travelled',
-        'days until arrival',
+  type: 'pie',
+  data: {
+    labels: [
+      'days travelled',
+      'days until arrival',
+    ],
+    datasets: [{
+      label: 'days',
+      borderColor: '#000000',
+      backgroundColor: [
+        orangeGrad,
+        blueGrad,
       ],
-      datasets: [
-        {
-          label: 'days',
-          borderColor: '#000000',
-          backgroundColor: [
-            orangeGrad,
-            blueGrad,
-          ],
-          data: [150, 60]
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-    },
-  });
+      data: [150, 60]
+    }]
+  },
+  options: {
+    responsive: true,
+  },
+});
 
 // Water available
 var ctx = document.getElementById('shipChartWaterStore').getContext('2d');
@@ -130,27 +125,25 @@ var blueGrad = ctx.createLinearGradient(0, 0, 0, 200);
 blueGrad.addColorStop(0, "#44426e");
 blueGrad.addColorStop(1, "#ad627f");
 var chartWaterStore = new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: [
-        'unused water',
-        'recycled water',
-        'unusable water',
+  type: 'pie',
+  data: {
+    labels: [
+      'unused water',
+      'recycled water',
+      'unusable water',
+    ],
+    datasets: [{
+      label: 'Hamsters',
+      borderColor: '#000000',
+      backgroundColor: [
+        orangeGrad,
+        redGrad,
+        blueGrad,
       ],
-      datasets: [
-        {
-          label: 'Hamsters',
-          borderColor: '#000000',
-          backgroundColor: [
-            orangeGrad,
-            redGrad,
-            blueGrad,
-          ],
-          data: [1100, 400, 100]
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-    },
-  });
+      data: [1100, 400, 100]
+    }]
+  },
+  options: {
+    responsive: true,
+  },
+});
